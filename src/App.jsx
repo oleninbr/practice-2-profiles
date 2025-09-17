2d4b1121b9f95dd2df8197243c33cb46de09c694
Bohdan Olenin <bohdan.olenin@oa.edu.ua>
2025-09-18 00:08:05 +0300

    Implement fetchUsers function to retrieve and format user data for profiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-// App.jsx
 import { useEffect, useState } from "react";
 import "./App.css";
-import Profile from "./components/Profile.jsx";
+import Profile from "./components/Profile";
+import { fetchUsers } from "./services/users";
 
 function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
-        // Додаємо role і avatarUrl до кожного юзера
-        const extendedUsers = data.slice(0, 7).map((user, index) => ({
-          name: user.name,
-          role: ["CEO", "Manager", "Designer", "Developer", "QA Engineer", "Analyst", "Intern"][index % 7],
-          avatarUrl: `https://i.pravatar.cc/150?img=${index + 1}`,
-        }));
-        setUsers(extendedUsers);
-      });
+    fetchUsers().then(setUsers);
   }, []);
 
   return (
